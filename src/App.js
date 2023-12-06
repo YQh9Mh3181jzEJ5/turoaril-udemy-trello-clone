39a3dcad9b9b2669d79377fe5871e6c414d4cadc
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2023-12-06 22:16:54 +0900

    add: filter関数

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ function App() {
     { name: "Jiro", age: 21 },
   ];
 
+  const words = [
+    "apple",
+    "banana",
+    "pineapple",
+    "orange",
+    "grape",
+    "peach",
+    "lemon",
+  ];
+
+  const result = words.filter((word) => word.length < 6);
+
   return (
     <div>
+      {/* map関数 */}
       <h1>map-practice</h1>
       {list.map((number) => (
         <div>{number * 2}</div>
@@ -17,6 +30,12 @@ function App() {
           <span> {data.age}</span>
         </div>
       ))}
+      <hr />
+      {/* filter関数 */}
+      {/* 条件を満たした要素だけを残す関数 */}
+      {/* 条件を満たさない要素を削除する */}
+      <h1>filter-practice</h1>
+      {console.log(result)}
     </div>
   );
 }
