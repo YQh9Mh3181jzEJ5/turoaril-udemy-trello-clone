5b61e3e22367a39ed4baea24748ced6993e646db
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2023-12-06 22:08:50 +0900

    add: code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <div></div>;
+  const list = [1, 2, 3, 4];
+  const datalist = [
+    { name: "Taro", age: 20 },
+    { name: "Jiro", age: 21 },
+  ];
+
+  return (
+    <div>
+      <h1>map-practice</h1>
+      {list.map((number) => (
+        <div>{number * 2}</div>
+      ))}
+      {datalist.map((data) => (
+        <div>
+          {data.name}
+          <span> {data.age}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
